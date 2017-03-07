c878d8100ebe73205bba1cc857475d74ef6f01cb
Maryam Younesi <maryam.younesi@outlook.com>
2017-03-07 19:56:10 +0330

    Getting description added

--- Main.java
+++ Main.java
@@ -3,30 +3,53 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlButton;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import java.util.logging.Logger;
+
+public static final string login_url = "https://www.appannie.com/account/login/?_ref=header";
+public static final string base_url = "https://www.appannie.com/apps/google-play/app/"
 
 public class Main {
-
-	public static void main(String[] args){  
-		// turn off htmlunit warnings
-    	java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(java.util.logging.Level.OFF);
-    	java.util.logging.Logger.getLogger("org.apache.http").setLevel(java.util.logging.Level.OFF);
+	
+	
+	WebClient webClient = new WebClient(BrowserVersion.CHROME);
+	
+	public static void main(String[] args){ 
+		
         
+		// Turn off htmlunit warnings
+    	getLogger("com.gargoylesoftware.htmlunit").setLevel(java.util.logging.Level.OFF);
+    	getLogger("org.apache.http").setLevel(java.util.logging.Level.OFF);      
+    	
+    	// Setting webClient options
+    	webClient.getOptions().setJavaScriptEnabled(true);
+    	webClient.waitForBackgroundJavaScript(3000);
     	
     	HtmlPage page = login();
-        System.out.println(page.asText());
+        
+		get_app_details("org.telegram.messenger");
 	} 
 	
+	
 	public static HtmlPage login(){
-		WebClient webClient = new WebClient(BrowserVersion.CHROME);
-
-        HtmlPage page = (HtmlPage) webClient.getPage("https://www.appannie.com/account/login/?_ref=header"); 
-        HtmlForm form = page.getFirstByXPath("//*[@id='login-form']");
+		   
+        HtmlPage login_page = (HtmlPage) webClient.getPage(login_url); 
+        HtmlForm form = login_page.getFirstByXPath("//*[@id='login-form']");
         form.getInputByName("username").setValueAttribute("younesi@ce.sharif.edu");
         form.getInputByName("password").setValueAttribute("Une30316");
         
         HtmlButton button = form.getFirstByXPath("//*[@id='submit']");
-        page = button.click();
-        return page;
+        HtmlPage result_page = button.click();
+        return result_page;
+	}
+	
+	
+	public static void get_app_details(string package_name){
+		
+		string detail_url = base_url + package_name + "/details/";
+		HtmlPage detail_Page = webClient.getPage(detail_url);
+        HtmlDivision div = (HtmlDivision) detail_Page.getFirstByXPath("//div[@class='desc']");
+        System.out.println("Description");
+        System.out.println(div.asText());
 	}
 
 }
---
+++ Review.java
@@ -0,0 +1,8 @@
+
+public class Review {
+	private string title;
+	private string body;
+	private Datetime date;
+	private int rating;
+	private Application application;
+}
